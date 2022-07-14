2ab5dd4b27db8562173cdf1427eac86a61a9af13
Rama Krishna <arkreddy2002@gmail.com>
2022-07-14 09:34:12 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import './App.css'
+import {Text} from '@mantine/core';
+
+function App() {
+
+  return (
+    <Text>Welcome to Reddit!</Text>
+  )
+}
+
+export default App
